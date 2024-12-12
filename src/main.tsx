886e21c9912cc3492dcffe9272342ebe057bb5e2
abhiram-ar <abhiram2001.ars@gmail.com>
2024-12-12 19:35:38 +0530

    pretty

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignInWithGoogle from "./components/auth/SignInWithGoogle";
-import {Provider} from "react-redux"
+import { Provider } from "react-redux";
 import store from "./redux/store";
 
 const appRouter = createBrowserRouter([
@@ -14,15 +14,14 @@ const appRouter = createBrowserRouter([
     },
     {
         path: "/test",
-        element: <SignInWithGoogle/>,
+        element: <SignInWithGoogle />,
     },
 ]);
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Provider store={store}>
-
-        <RouterProvider router={appRouter} />
+            <RouterProvider router={appRouter} />
         </Provider>
     </StrictMode>
 );
