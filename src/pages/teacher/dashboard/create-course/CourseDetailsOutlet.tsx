ceaae1f288e20f8f579625cf104f060e9ca2cc6f
abhiram-ar <abhiram2001.ars@gmail.com>
2024-12-22 01:15:14 +0530

    added courseDeails form

--- src/pages/teacher/dashboard/create-course/CourseDetailsOutlet.tsx
+++ src/pages/teacher/dashboard/create-course/CourseDetailsOutlet.tsx
@@ -14,6 +14,10 @@ const CourseDetailsOutlet: React.FC = () => {
         defaultValues: {
             title: courseDetails.title,
             description: courseDetails.description,
+            price: courseDetails.price,
+            estimatedPrice: courseDetails.estimatedPrice,
+            category: courseDetails.category,
+            level: courseDetails.level,
         },
     });
 
@@ -96,11 +100,166 @@ const CourseDetailsOutlet: React.FC = () => {
                     />
                 </div>
 
+                {/* price group */}
+                <div className="grid grid-cols-2 gap-5 mt-5">
+                    {/* price */}
+                    <div>
+                        <label
+                            htmlFor="price"
+                            className="font-semibold flex justify-between gap-2 items-baseline"
+                        >
+                            {" "}
+                            <div className="flex gap-2 items-baseline">
+                                Selling price in rupees
+                                {errors.price && (
+                                    <span className="validation-error">
+                                        ({String(errors.price.message)})
+                                    </span>
+                                )}
+                            </div>
+                        </label>
+
+                        <input
+                            type="number"
+                            {...register("price", {
+                                required: "required",
+                                max: {
+                                    value: 10000,
+                                    message: "Price cannot be more than 10,000",
+                                },
+                                min: {
+                                    value: 0,
+                                    message: "Price cannot be less than 0",
+                                },
+                            })}
+                            id="price"
+                            className="input-neo w-full"
+                            placeholder="eg: 399"
+                        />
+                    </div>
+
+                    {/* extimated price */}
+                    <div>
+                        <label
+                            htmlFor="estimatedPrice"
+                            className="font-semibold flex justify-between gap-2 items-baseline"
+                        >
+                            {" "}
+                            <div className="flex gap-2 items-baseline">
+                                Estimated price
+                                {errors.estimatedPrice && (
+                                    <span className="validation-error">
+                                        ({String(errors.estimatedPrice.message)}
+                                        )
+                                    </span>
+                                )}
+                            </div>
+                        </label>
+
+                        <input
+                            type="number"
+                            {...register("estimatedPrice", {
+                                required: "required",
+                                max: {
+                                    value: 10000,
+                                    message: "Price cannot be more than 10,000",
+                                },
+                                min: {
+                                    value: 0,
+                                    message: "Price cannot be less than 0",
+                                },
+                            })}
+                            id="estimatedPrice"
+                            className="input-neo w-full"
+                            placeholder="eg: 2099"
+                        />
+                    </div>
+                </div>
+
+                {/* category level grooup */}
+
+                <div className="grid grid-cols-2 gap-5 mt-5">
+                    {/* category */}
+                    <div>
+                        <label
+                            htmlFor="category"
+                            className="font-semibold flex justify-between gap-2 items-baseline"
+                        >
+                            {" "}
+                            <div className="flex gap-2 items-baseline">
+                                Category
+                                {errors.category && (
+                                    <span className="validation-error">
+                                        ({String(errors.category.message)})
+                                    </span>
+                                )}
+                            </div>
+                        </label>
+
+                        <select
+                            {...register("category", {
+                                required: "Select a category",
+                            })}
+                            defaultValue=""
+                            className="input-neo appearance-none w-full"
+                        >
+                            <option value="" disabled>
+                                Select a category
+                            </option>
+                            <option value="softwareDevelopment">
+                                Sofware Development
+                            </option>
+                            <option value="AIandMachineLearning">
+                                AI and Machine Learning
+                            </option>
+                            <option value="Pychology">Pychology</option>
+                            <option value="healthAndWellness">
+                                Health and Wellness
+                            </option>
+                            <option value="personalDevelopment">
+                                Personal Development
+                            </option>
+                        </select>
+                    </div>
+
+                    {/* level */}
+                    <div>
+                        <label
+                            htmlFor="level"
+                            className="font-semibold flex justify-between gap-2 items-baseline"
+                        >
+                            {" "}
+                            <div className="flex gap-2 items-baseline">
+                                level
+                                {errors.level && (
+                                    <span className="validation-error">
+                                        ({String(errors.level.message)})
+                                    </span>
+                                )}
+                            </div>
+                        </label>
+
+                        <select
+                            {...register("level", {
+                                required: "Select a level",
+                            })}
+                            defaultValue=""
+                            className="input-neo appearance-none w-full"
+                        >
+                            <option value="" disabled>
+                                Select a level
+                            </option>
+                            <option value="beginner">Beginner</option>
+                            <option value="intermediate">Intermediate</option>
+                            <option value="advanced">Advanced</option>
+                        </select>
+                    </div>
+                </div>
                 <Button
                     disabled={Object.keys(errors).length !== 0 ? true : false}
                     type="submit"
                     variant="reverse"
-                    className="bg-green-50 absolute right-0 mt-5"
+                    className="bg-green-300 absolute right-0 mt-5"
                 >
                     Save and Next
                 </Button>
