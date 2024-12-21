00f81393faa080021492f92f30f1a98a9e64ec8d
abhiram-ar <abhiram2001.ars@gmail.com>
2024-12-21 21:08:28 +0530

    creaated draft navabr

--- src/pages/teacher/dashboard/create-course/CourseDraft.tsx
+++ src/pages/teacher/dashboard/create-course/CourseDraft.tsx
@@ -1,16 +1,26 @@
 import { useGetFullCourseDetailsQuery } from "@/redux/features/teacher/courseCreationAPIs";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const CourseDraft: React.FC = () => {
     const { id } = useParams();
-    const { data } = useGetFullCourseDetailsQuery({ courseId: id });
+    const { data: courseDetails } = useGetFullCourseDetailsQuery({
+        courseId: id,
+    });
 
+    console.log(courseDetails);
 
-    console.log(id);
-    console.log(data);
-
-    return <div>CourseDraft</div>;
+    return (
+        <div>
+            <nav className="w-4/5 mx-auto rounded-base bg-zinc-100 border border-black box-border grid grid-cols-4 overflow-hidden">
+                <Link className="hover:bg-zinc-300 text-center p-3 transition-all duration-300">Course Basic</Link>
+                <Link className="hover:bg-zinc-300 text-center p-3 transition-all duration-300">Course Structure</Link>
+                <Link className="hover:bg-zinc-300 text-center p-3 transition-all duration-300">Course Assets</Link>
+                <Link className="hover:bg-zinc-300 text-center p-3 transition-all duration-300">Publish</Link>
+            
+            </nav>
+        </div>
+    );
 };
 
 export default CourseDraft;
