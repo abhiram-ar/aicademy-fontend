8b98d1b2f8ce36488044b87db409195a7a733799
abhiram-ar <abhiram2001.ars@gmail.com>
2024-12-23 18:20:00 +0530

    video presigned upload axios

--- src/pages/teacher/dashboard/create-course/CourseAssetsOutlet.tsx
+++ src/pages/teacher/dashboard/create-course/CourseAssetsOutlet.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { RootState } from "@/redux/store";
+import axios from "axios";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const CourseAssetsOutlet = () => {
-  return (
-    <div>CourseAssetsOutlet</div>
-  )
-}
+    const token = useSelector((state: RootState) => state.auth.token);
+    const [isUploading, setIsUploading] = useState<boolean>(false);
 
-export default CourseAssetsOutlet
+    const handleUpload = async (file: File) => {
+        try {
+            const { data } = await axios.post(
+                "http://localhost:3000/api/course/upload/presignedurl",
+                {
+                    fileName: file.name,
+                    fileType: file.type,
+                },
+                {
+                    headers: { Authorization: `Bearer ${token}` },
+                }
+            );
+            console.log(data);
+            const res = await axios.put(data.preSignedURL, file, {
+                headers: { "Content-Type": file.type },
+            });
+
+            console.log(res);
+        } catch (error) {
+            console.error(`error while uploading`, error);
+        }
+    };
+    return (
+        <div>
+            <input
+                type="file"
+                onChange={(e) => {
+                    console.log(e.target.files);
+                    if (e.target.files && e.target?.files?.length > 0) {
+                        handleUpload(e.target.files[0]);
+                    }
+                }}
+            />
+        </div>
+    );
+};
+
+export default CourseAssetsOutlet;
