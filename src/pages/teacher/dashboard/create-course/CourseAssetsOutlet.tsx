54232e531dfdce99657f9446abfe4b3c0acf952c
abhiram-ar <abhiram2001.ars@gmail.com>
2024-12-23 18:54:18 +0530

    uploading and cancellation

--- src/pages/teacher/dashboard/create-course/CourseAssetsOutlet.tsx
+++ src/pages/teacher/dashboard/create-course/CourseAssetsOutlet.tsx
@@ -1,15 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { RootState } from "@/redux/store";
 import axios from "axios";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 const CourseAssetsOutlet = () => {
     const token = useSelector((state: RootState) => state.auth.token);
     const [isUploading, setIsUploading] = useState<boolean>(false);
+    const [uploadProgress, setUploadProgress] = useState<number>(0);
+    const abortController = useRef<AbortController | null>(null);
 
     const handleUpload = async (file: File) => {
         try {
+            setIsUploading(true);
+            setUploadProgress(0);
+
             const { data } = await axios.post(
                 "http://localhost:3000/api/course/upload/presignedurl",
                 {
@@ -20,16 +25,46 @@ const CourseAssetsOutlet = () => {
                     headers: { Authorization: `Bearer ${token}` },
                 }
             );
+
             console.log(data);
+
+            abortController.current = new AbortController();
+
             const res = await axios.put(data.preSignedURL, file, {
                 headers: { "Content-Type": file.type },
+                onUploadProgress: (event) => {
+                    const percentComplete = Math.round(
+                        (event.loaded * 100) / event.total
+                    );
+                    setUploadProgress(percentComplete);
+                },
+                signal: abortController.current.signal,
             });
 
             console.log(res);
+            console.log(`mock uploading to DB..`);
         } catch (error) {
-            console.error(`error while uploading`, error);
+            if (axios.isCancel(error)) {
+                console.log(`upload cancelled`);
+                //todo: toast
+            } else {
+                console.error(`error while uploading`, error);
+            }
+        } finally {
+            console.log(`finally`);
+            setUploadProgress(0);
+            setIsUploading(false);
+            abortController.current = null;
+            //set about controller null
         }
     };
+
+    const handleUploadCancel = () => {
+        if (abortController.current) {
+            abortController.current.abort();
+        }
+    };
+
     return (
         <div>
             <input
@@ -41,6 +76,12 @@ const CourseAssetsOutlet = () => {
                     }
                 }}
             />
+            {isUploading && (
+                <>
+                    <p>{uploadProgress}% uploaded</p>
+                    <button onClick={handleUploadCancel}>cancel</button>
+                </>
+            )}
         </div>
     );
 };
