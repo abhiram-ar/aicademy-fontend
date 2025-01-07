58c87a2461a84a643c973a717586e4dbe98a1c7f
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-07 16:48:56 +0530

    price secction

--- src/pages/landing/sections/perks/PerksSection.tsx
+++ src/pages/landing/sections/perks/PerksSection.tsx
@@ -1,6 +1,8 @@
 import quizesArt from "./../../../../assets/quizesArt.png";
 import communityArt from "./../../../../assets/communityArt.png";
 import certificationArt from "./../../../../assets/certificationArt.png";
+import AIChatExample from "./../../../../assets/AIConvExample.png";
+import afforfablePricingArt from "./../../../../assets/AffrodablePricingArt.png";
 
 const PerksSection = () => {
     const primaryPerks: {
@@ -50,7 +52,7 @@ const PerksSection = () => {
                 {primaryPerks.map((perk, index) => (
                     <div
                         key={index}
-                        className={`bg-[${perk.color}] border-4 border-black h-fit rounded-base  p-8 text-center`}
+                        className={`bg-[${perk.color}] border-4 border-black h-full rounded-base  p-8 px-5 lg:px-12 text-center`}
                     >
                         <div className="size-20 mx-auto">
                             <img src={perk.icon} alt={perk.heading} />
@@ -62,13 +64,18 @@ const PerksSection = () => {
             </div>
 
             {/* ai feature */}
-            <div className="border-4 border-black mt-5 rounded-base">
+            <div
+                className="border-4 border-black mt-5 rounded-base bg-[#212121] text-darkText px-20 py-5 font-publicSans grid grid-col-3 lg:grid-cols-5 gap-10
+            "
+            >
                 {/* content */}
-                <div>
+                <div className="col-span-3 py-12">
                     {/* info */}
                     <div>
-                        <h3>Get Instant Answers, Right from the Source!</h3>
-                        <p>
+                        <h3 className="text-2xl font-bold">
+                            Get Instant Answers, Right from the Source!
+                        </h3>
+                        <p className="text-zinc-300 w-10/12">
                             Our advanced AI helps you ask questions directly
                             related to a video’s content. Whether it's a
                             lecture, tutorial, or presentation, the AI analyzes
@@ -78,12 +85,12 @@ const PerksSection = () => {
                     </div>
 
                     {/* insigt demo */}
-                    <div>
-                        <p>
+                    <div className="grid grid-rows-2 w-10/12 mt-12 font-mono">
+                        <p className="ms-auto text-right w-3/5 text-[#499FFB]">
                             Simply type your question, and the AI will refer to
                             the teacher's statements in the video
                         </p>
-                        <p>
+                        <p className="text-left w-3/5 text-[#FFDC58]">
                             Get reliable and clear responses, saving you time
                             and boosting your understanding.
                         </p>
@@ -91,7 +98,31 @@ const PerksSection = () => {
                 </div>
 
                 {/* ai example image */}
-                <div></div>
+                <div className="col-span-3 lg:col-span-2">
+                    <img
+                        src={AIChatExample}
+                        className="w-full h-full object-fill"
+                        alt=""
+                    />
+                </div>
+            </div>
+
+            {/* pricing */}
+            <div className="border-4 border-black mt-5 rounded-base bg-[#a3e636] grid grid-cols-6 px-20 py-10">
+                {/* icon */}
+                <div className="col-span-2 ">
+                    <img src={afforfablePricingArt} className="w-full h-full px-10 object-scale-down" alt="" />
+                </div>
+
+                <div className="p-20 col-span-4">
+                    <h3>Learning Made Affordable for Everyone</h3>
+                    <p>
+                        Access high-quality courses and tools at prices that fit
+                        your budget.
+                        <br /> Unlock unlimited learning without breaking the
+                        bank
+                    </p>
+                </div>
             </div>
         </div>
     );
