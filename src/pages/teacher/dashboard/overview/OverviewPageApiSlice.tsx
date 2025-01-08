e07cfbb3625caf980c7f8faba954306e7b4773f8
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-08 14:24:09 +0530

    added live api for revenue

---
+++ src/pages/teacher/dashboard/overview/OverviewPageApiSlice.tsx
@@ -0,0 +1,12 @@
+import apiSlice from "@/redux/features/api/apiSlice";
+
+const teacherOverviewPageApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getRevenuelastTwoMonth: builder.query({
+            query: () => "/api/teacher/dashboard/revenue",
+            providesTags: ["teacherDashboard"],
+        }),
+    }),
+});
+
+export const { useGetRevenuelastTwoMonthQuery } = teacherOverviewPageApiSlice;
--- src/pages/teacher/dashboard/overview/RevenueCard.tsx
+++ src/pages/teacher/dashboard/overview/RevenueCard.tsx
@@ -1,9 +1,54 @@
+import { useGetRevenuelastTwoMonthQuery } from "./OverviewPageApiSlice";
+
 const RevenueCard = () => {
+    const { data } = useGetRevenuelastTwoMonthQuery({});
     return (
         <div className="border bg-white p-8 font-publicSans rounded-base">
             <h3>Revenue</h3>
-            <h2 className="text-xl font-semibold mt-3">₹15,000</h2>
-            <p>20% up from last month</p>
+            <h2 className="text-xl font-semibold mt-3">
+                {data ? (
+                    data.revenue.currentMonth.toLocaleString("en-IN", {
+                        style: "currency",
+                        currency: "INR",
+                    })
+                ) : (
+                    <span className="min-w-20 inline-block rounded-base bg-slate-200  ms-2 animate-pulse text-transparent h-6">
+                        .
+                    </span>
+                )}
+            </h2>
+            <p>
+                {" "}
+                {data ? (
+                    data.revenue.currentMonth - data.revenue.prevMonth > 0 ? (
+                        <p className="text-green-600">
+                            {" "}
+                            {(
+                                ((data.revenue.currentMonth -
+                                    data.revenue.prevMonth) /
+                                    data.revenue.prevMonth) *
+                                100
+                            ).toFixed(2)}
+                            % up from last month
+                        </p>
+                    ) : (
+                        <p className="text-red-600">
+                            {" "}
+                            {(
+                                ((data.revenue.prevMonth -
+                                    data.revenue.currentMonth) /
+                                    data.revenue.prevMonth) *
+                                100
+                            ).toFixed(2)}
+                            % down from last month
+                        </p>
+                    )
+                ) : (
+                    <span className="min-w-20 inline-block rounded-base bg-slate-200  ms-2 animate-pulse text-transparent">
+                        .
+                    </span>
+                )}
+            </p>
         </div>
     );
 };
