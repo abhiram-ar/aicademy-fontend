4ec6c21a77da1082c1dab95bfef306c07907a72a
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-10 15:02:37 +0530

    overview template

--- src/pages/admin/Overview/AdminOverviewPage.tsx
+++ src/pages/admin/Overview/AdminOverviewPage.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import TotalEarningsCard from "./TotalEarningsCard";
+import RevenueCard from "./RevenueCard";
+import PurchasesCard from "./PurchasesCard";
+import BarChart from "./BarChart";
 
 const AdminOverviewPage = () => {
     return (
@@ -6,7 +10,16 @@ const AdminOverviewPage = () => {
             <h2 className="font-medium text-2xl bg-zinc-300 px-2 rounded-base w-fit -mt-12 ms-2">
                 Overview
             </h2>
-            <div className="border w-10/12 mx-auto mt-5"></div>
+            <div className="border w-10/12 mx-auto mt-5">
+                <div className="grid grid-cols-3 gap-5">
+                    <RevenueCard />
+                    <PurchasesCard />
+                    <TotalEarningsCard />
+                </div>
+                <div className="mt-5">
+                    <BarChart />
+                </div>
+            </div>
         </div>
     );
 };
