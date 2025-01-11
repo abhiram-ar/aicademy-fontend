50361ddee66cb6e5f417cf3e8cf8b9f4ce388596
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-11 12:33:47 +0530

    title fix

--- src/pages/admin/Revenue/RevenuePage.tsx
+++ src/pages/admin/Revenue/RevenuePage.tsx
@@ -4,7 +4,7 @@ const AdminRevenuePage = () => {
     return (
         <div className="mx-10">
             <h2 className="font-medium text-2xl bg-zinc-300 px-2 rounded-base w-fit -mt-12 ms-2">
-                User Reports
+               Revenue 
             </h2>
             <div className="w-10/12 mx-auto mt-5">
                 <div>
