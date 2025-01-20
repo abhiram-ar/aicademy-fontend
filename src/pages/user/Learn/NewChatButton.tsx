e3a5ae0180b82d621971a3fe3e57cf558f9c4e67
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-20 15:38:08 +0530

    new chat hover

--- src/pages/user/Learn/NewChatButton.tsx
+++ src/pages/user/Learn/NewChatButton.tsx
@@ -8,11 +8,11 @@ import {
 
 const NewChatButton = () => {
     return (
-        <div className="absolute top-2 right-2">
+        <div className="absolute top-2 right-3">
             <TooltipProvider delayDuration={200}>
                 <Tooltip>
                     <TooltipTrigger>
-                        <SquarePlus />
+                        <SquarePlus className="fill-zinc-300 stroke-black/70 hover:stroke-black" />
                     </TooltipTrigger>
                     <TooltipContent>
                         <p className="font-publicSans">New chat</p>
