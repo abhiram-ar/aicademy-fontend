e4705d5f14ae5499b407415d0b614ba62edfd150
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-22 14:21:01 +0530

    accoridan open the current playing video

--- src/pages/user/Learn/CourseContents.tsx
+++ src/pages/user/Learn/CourseContents.tsx
@@ -21,50 +21,64 @@ const CourseContents: React.FC<Props> = ({
     setCurrentlyPlaying,
     currentlyPlaying,
 }) => {
+    const currentchapter = (lessonId?: string): string[] => {
+        if (!lessonId || !chapters || !currentlyPlaying) return [""];
+
+        const chapter = chapters.find((chapter) =>
+            chapter.lessons.find((lesson) => lesson._id === lessonId)
+        );
+        return chapter ? [chapter._id] : [""];
+    };
     return (
-        <Accordion className="p-2" type="multiple">
-            {chapters &&
-                chapters.map((chapter) => (
-                    <AccordionItem
-                        key={chapter._id}
-                        className="shadow-none border-2 mt-2"
-                        value={`item-${chapter._id}`}
-                    >
-                        <AccordionTrigger className="bg-zinc-300 font-semibold">
-                            {chapter.chapterTitle}
-                        </AccordionTrigger>
-                        <AccordionContent className="p-0">
-                            <div className="">
-                                {chapter.lessons.map((lesson) => (
-                                    <p
-                                        key={lesson._id}
-                                        className={`flex gap-2 items-center border p-2.5  hover:underline hover:bg-slate-200 cursor-pointer  ${
-                                            currentlyPlaying &&
-                                            currentlyPlaying._id ===
-                                                lesson._id &&
-                                            "bg-amber-300 hover:bg-amber-300"
-                                        }`}
-                                        onClick={() =>
-                                            setCurrentlyPlaying(lesson)
-                                        }
-                                    >
-                                        <Video
-                                            className={`size-4 ${
+        <div className="w-full max-h-[79.5vh] overflow-y-auto">
+            <Accordion
+                className="p-2"
+                type="multiple"
+                defaultValue={[`item-${currentchapter(currentlyPlaying?._id)}`]}
+            >
+                {chapters &&
+                    chapters.map((chapter) => (
+                        <AccordionItem
+                            key={chapter._id}
+                            className="shadow-none border-2 mt-2"
+                            value={`item-${chapter._id}`} //id for accordian open close tracking, open currently playing on mount
+                        >
+                            <AccordionTrigger className="bg-zinc-300 font-semibold">
+                                {chapter.chapterTitle}
+                            </AccordionTrigger>
+                            <AccordionContent className="p-0">
+                                <div className="">
+                                    {chapter.lessons.map((lesson) => (
+                                        <p
+                                            key={lesson._id}
+                                            className={`flex gap-2 items-center border p-2.5  hover:underline hover:bg-slate-200 cursor-pointer  ${
                                                 currentlyPlaying &&
                                                 currentlyPlaying._id ===
                                                     lesson._id &&
-                                                "fill-slate-600"
+                                                "bg-amber-300 hover:bg-amber-300"
                                             }`}
-                                        />
+                                            onClick={() =>
+                                                setCurrentlyPlaying(lesson)
+                                            }
+                                        >
+                                            <Video
+                                                className={`size-4 ${
+                                                    currentlyPlaying &&
+                                                    currentlyPlaying._id ===
+                                                        lesson._id &&
+                                                    "fill-slate-600"
+                                                }`}
+                                            />
 
-                                        {lesson.lessonTitle}
-                                    </p>
-                                ))}
-                            </div>
-                        </AccordionContent>
-                    </AccordionItem>
-                ))}
-        </Accordion>
+                                            {lesson.lessonTitle}
+                                        </p>
+                                    ))}
+                                </div>
+                            </AccordionContent>
+                        </AccordionItem>
+                    ))}
+            </Accordion>
+        </div>
     );
 };
 
