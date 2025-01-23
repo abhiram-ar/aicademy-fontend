4615dd00492e4e0a2baf854fc491253807c53084
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-23 17:12:28 +0530

    order page

--- src/pages/user/myLearning/MyLearning.tsx
+++ src/pages/user/myLearning/MyLearning.tsx
@@ -4,7 +4,7 @@ const MyLearning = () => {
     return (
         <div>
             <div className="w-full min-h-screen bg-paperYellow">
-                <div className="w-11/12 md:10/12 xl:w-9/12 mx-auto py-10">
+                <div className="w-11/12 xl:w-9/12 mx-auto py-10">
                     <h2 className="bg-zinc-400 text-xl font-semibold w-fit px-1 rounded-base">
                         My Courses
                     </h2>
