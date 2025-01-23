a22bd514f6d80a881678ec21616b2ed89f468f3a
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-23 18:40:12 +0530

    hero section fix

--- src/pages/landing/sections/HeroSection.tsx
+++ src/pages/landing/sections/HeroSection.tsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 
 const HeroSection = () => {
     return (
-        <div className="w-10/12 md:w-8/12 mx-auto grid grid-2 lg:grid-cols-10 gap-10 py-16 mb-4">
-            <div className=" col-span-7 lg:col-span-6 mt-2 md:mt-10 ">
-                <div>
-                    <h1 className="font-RedHat text-2xl md:text-[3rem] leading-none font-extrabold">
+        <div className="w-11/12 lg:w-10/12 xl:w-8/12 mx-auto grid grid-cols-10 gap-10 py-16 mb-4 ">
+            <div className="col-span-10 md:col-span-6 lg:col-span-6 mt-2 md:mt-10 ">
+                <div className="w-fit">
+                    <h1 className="font-RedHat text-2xl md:text-4xl xl:text-[3rem] leading-none font-extrabold w-fit">
                         Revolutionize Learning
                         <br /> with Adaptive Intelligence.
                     </h1>
-                    <p className="font-publicSans text mt-5 font-medium w-80 md:w-[30rem]">
+                    <p className="font-publicSans text mt-5 font-medium xl:w-[30rem]">
                         Experience a powerful LMS that blends advanced Al with{" "}
                         robust tools for seamless leaming. Whether you're a
                         student aiming for mastery, an educator designing
@@ -19,7 +19,7 @@ const HeroSection = () => {
                         training, our LMS empowers you to achieve more with
                         smart, efficient, and engaging solutions.
                     </p>
-                    <div className="font-publicSans mt-16">
+                    <div className="font-publicSans mt-16 w-fit">
                         <p className="my-2">
                             Discover the future of Learning-Today
                         </p>
@@ -35,7 +35,7 @@ const HeroSection = () => {
                 </div>
             </div>
 
-            <div className="w-80 md:w-[35rem] col-span-3 lg:col-span-4">
+            <div className="col-span-10 md:col-span-4 lg:col-span-4  xl:w-[35rem] ">
                 <img src={heroart} className="object-fill" alt="" />
             </div>
         </div>
