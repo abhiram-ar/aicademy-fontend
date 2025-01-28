fdc6b1e40f0bb71feed2c2dc171e981287a1a479
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-28 15:13:17 +0530

    usebebound type fix

--- src/pages/teacher/dashboard/payout/BankVerification.tsx
+++ src/pages/teacher/dashboard/payout/BankVerification.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { loadScript } from "@/utils/loadscript";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import {
     useCreateBankVerificationOrderMutation,
     useVerifyTeacherBankAccountMutation,
