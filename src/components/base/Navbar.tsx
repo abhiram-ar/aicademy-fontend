7e21ab273f9849601047834d3c67e62cc0a879fb
abhiram-ar <abhiram2001.ars@gmail.com>
2025-01-29 20:10:35 +0530

    logo position

--- src/components/base/Navbar.tsx
+++ src/components/base/Navbar.tsx
@@ -4,7 +4,7 @@ type Props = {children?: React.ReactNode, justify?: string}
 
 const Navbar: React.FC<Props> = ({children, justify="between"}) => {
     return (
-        <div className={`w-full h-24 bg-white px-10 shadow-[0_4px] flex justify-${justify} items-center relative z-20`}>
+        <div className={`w-full h-24 bg-white px-5 lg:px-10 shadow-[0_4px] flex justify-${justify} items-center relative z-20`}>
             {children}
         </div>
     );
